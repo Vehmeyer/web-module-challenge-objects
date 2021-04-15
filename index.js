5206cde67aa18208a57c9f026e5bad245294c515
John Vehmeyer <j.vehmeyer@gmail.com>
2021-04-15 15:14:54 -0500

    completed task 7

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ Use the getLastReview function below to do the following:
 
 
 function getLastReview(array) {
-  return array[array.length-1].toString(`${array[array.length-1].name} gave the restaurant a ${array[array.length-1].rating} star review, and their feedback was: ${array[array.length-1].feedback}`); 
+  return `${array[array.length-1].name} gave the restaurant a ${array[array.length-1].rating} star review, and their feedback was: ${array[array.length-1].feedback}`; 
 } 
 
 getLastReview(reviews);
